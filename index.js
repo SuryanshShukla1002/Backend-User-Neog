a0c75da8775b3cfe4bb285e58fe951ad8938b027
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-08-11 03:22:36 +0530

    fix: vercel problem:

--- index.js
+++ index.js
@@ -6,41 +6,11 @@ app.use(cors());
 app.use(express.json());
 
 const users = [
-    {
-        id: 1,
-        username: "octocat",
-        name: "The Octocat",
-        repoCount: 8,
-        location: "San Francisco",
-    },
-    {
-        id: 2,
-        username: "torvalds",
-        name: "Linus Torvalds",
-        repoCount: 25,
-        location: "Portland",
-    },
-    {
-        id: 3,
-        username: "gaearon",
-        name: "Dan Abramov",
-        repoCount: 50,
-        location: "London",
-    },
-    {
-        id: 4,
-        username: "addyosmani",
-        name: "Addy Osmani",
-        repoCount: 42,
-        location: "Mountain View",
-    },
-    {
-        id: 5,
-        username: "tj",
-        name: "TJ Holowaychuk",
-        repoCount: 150,
-        location: "Victoria",
-    },
+    { id: 1, username: "octocat", name: "The Octocat", repoCount: 8, location: "San Francisco" },
+    { id: 2, username: "torvalds", name: "Linus Torvalds", repoCount: 25, location: "Portland" },
+    { id: 3, username: "gaearon", name: "Dan Abramov", repoCount: 50, location: "London" },
+    { id: 4, username: "addyosmani", name: "Addy Osmani", repoCount: 42, location: "Mountain View" },
+    { id: 5, username: "tj", name: "TJ Holowaychuk", repoCount: 150, location: "Victoria" },
 ];
 
 app.get("/users", (req, res) => {
@@ -58,7 +28,4 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
